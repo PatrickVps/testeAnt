9fb5a4a7928c44e2e5c32e53f98c27d757c68d74
patrickvongpraseuth <vongpraseuth.p@sfeir.com>
2017-05-29 13:00:24 +0200

    add instrumented tests relatives to mobile device

--- app/src/androidTest/java/com/sfeir/testant/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/sfeir/testant/ExampleInstrumentedTest.java
@@ -2,9 +2,14 @@ package com.sfeir.testant;
 
 import android.content.Context;
 import android.content.pm.PackageManager;
+import android.net.wifi.WifiInfo;
+import android.net.wifi.WifiManager;
 import android.support.test.InstrumentationRegistry;
 import android.support.test.runner.AndroidJUnit4;
+import android.telephony.TelephonyManager;
 import android.util.Log;
+
+import junit.framework.Assert;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -25,9 +30,34 @@ public class ExampleInstrumentedTest {
 
         assertEquals("com.sfeir.testant", appContext.getPackageName());
 
+        ////////////////
+        //CHECK CAMERA AVAILABILITY
+        ////////////////
+
         PackageManager pm = appContext.getPackageManager();
 
         assertTrue(pm.hasSystemFeature(PackageManager.FEATURE_CAMERA));
+
+
+        ////////////////
+        //GET CARRIER NAME
+        ////////////////
+
+        TelephonyManager manager = (TelephonyManager)appContext.getSystemService(Context.TELEPHONY_SERVICE);
+        String carrierName = manager.getNetworkOperatorName();
+
+        assertEquals("",carrierName);
+
+
+        ////////////////
+        //CHECK WIFI NAME
+        ////////////////
+
+        WifiManager wifiMgr = (WifiManager) appContext.getSystemService(Context.WIFI_SERVICE);
+        WifiInfo wifiInfo = wifiMgr.getConnectionInfo();
+        String name = wifiInfo.getSSID();
+
+        assertEquals("\"SFEIR\"", name);
 
     }
 }
